6a64255aef0047b3aa4305c613e87421056af014
Anthony <anthonywendyantuesdelima@gmail.com>
2022-06-29 11:48:52 -0300

    Integração com API.

--- frontend/src/helpers/OlxAPI.js
+++ frontend/src/helpers/OlxAPI.js
@@ -1,7 +1,45 @@
+import Cookies from 'js-cookie';
+
+const BASEAPI = 'http://192.168.128.111:5000'
+
+const apiFetchPost = async (endpoint, body) => {
+    
+    if(!body.token){
+        let token = Cookies.get('token');
+        if(token) {
+            body.token  = token;
+        }
+    }
+    
+    const res = await fetch(BASEAPI+endpoint, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        boy: JSON.stringify(body)
+    });
+
+    const json = await res.json();
+
+    if(json.notallowed) {
+        window.location.href = '/singin';
+        return 
+    }
+
+    return json;
+}
+
+
+
 const OlxAPI = {
     login:async (email, password) => {
 
-        return {error: 'Cadastro incompleto.'};
+        const json = await apiFetchPost(
+                'user/singin',
+                {email, password}
+        );
+        return json;
     }
 };
 
