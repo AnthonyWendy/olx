cef57f13175ba50019d9a14454aaf584553794bc
Anthony <anthonywendyantuesdelima@gmail.com>
2022-07-15 12:58:51 -0300

    .

--- frontend/src/pages/Ads/index.js
+++ frontend/src/pages/Ads/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import { SearchArea,   PageArea} from './styled';
 import useApi from '../../helpers/OlxAPI'
 
@@ -9,6 +9,18 @@ import AdItem from '../../components/partiais/AdItem';
 
 const Page = () => {
     const api = useApi();
+
+    alert(useLocation().search);
+
+    const useQueryString = () => {
+        return new URLSearchParams( useLocation().search );
+    }
+
+    const query = useQueryString();
+    
+    const [q, setQ] = useState( query.get('q') != null ? query.get('q') : '');
+    const [cat, setCat] = useState( query.get('cat') != null ? query.get('cat') : '' );
+    const [state, setState] = useState( query.get('state') != null ? query.get('state') : '' );
 
     const [stateList, setStateList] = useState([]);
     const [categories, setCategories] = useState([]);
@@ -43,54 +55,43 @@ const Page = () => {
     }, []);
 
     return (
+        <PageContainer>
+            <PageArea>
+                <div className="leftSide">
+                    <form method="GET">
+                        <input 
+                            type="text" 
+                            name="q" 
+                            placeholder='O que você procura?'
+                            value={q}
+                        />
 
-        <>
-            <SearchArea>
-                <PageContainer>
-                    <div className="searchbox">
-                    <form>
-                        <input type="text" name="q" placecholder="O que você procura?"/>
-                        <select className="state">
-                            {stateList.map((i,k) => 
-                            <option key={k} value={i.name}>{i.name}</option>
+                        <div className="filterName">Estado:</div>
+                        <select name="state">
+                            <option></option>
+                            {stateList.map((i, k)=>
+                                <option key={k} value={i.name}>{i.name}</option>
                             )}
                         </select>
-                        <button>Pesquisar</button>
+
+                        <div className="filterName">Categorias:</div>
+                        <ul>
+                            {categories.map((i, k)=>
+                                <li key="k" className={cat==i.slug ? 'categoryItem active' : 'categoryItem'} >
+                                    <img src={i.img} alt=""/>
+                                    <span>{i.name}</span>
+
+                                </li>
+                            )}
+                        </ul>
                     </form>
+                </div>
 
-                    </div>
-                    <div className="categoryList">
-                        {categories.map((i, k) =>
-                            <Link key={k} to="`ads?cat=${i.slug}`" className="categoryItem">
-                                <img src={i.img} alt=""/>
-                                <span>{i.name}</span>
-                            </Link>
-                        )}
-                    </div>
-                </PageContainer>
-            </SearchArea>
-            <PageContainer>
-                <PageArea>
-                    <h2>Ánuncios Recentes</h2>
-
-                    <div className="list">
-                        {adList.map((i, k) =>
-                            {return <AdItem key={k} data={i} />}
-                        )}
-                    </div>
-
-                    <Link to="/ads" className="tonho">Ver todos</Link>
-
-                    <hr/>
-
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since
-the 1500s,when an unknown printer tookagalley of type and scrambled it to makeatype specimen book.It has survived not only five centuries.
-but also the leap into electronic typesetting,remaining essentially unchanged.</p>
-                </PageArea>
-            </PageContainer>
-            
-        </>
-
+                <div className="rightSide">
+                    right
+                </div>
+            </PageArea>
+        </PageContainer>
     );
 }
 export default Page;
--- frontend/src/pages/Ads/styled.js
+++ frontend/src/pages/Ads/styled.js
@@ -1,105 +1,63 @@
 import styled from 'styled-components';
 
-export const SearchArea = styled.div`
-    background-color: #DDD;
-    margin-bottom: 2px solid #CCC;
-    padding: 20px 0;
-
-    .searchbox {
-        background-color: #9BB83C;
-        padding: 20px 15px;
-        border-radius: 5px;
-        box-shadow: 1px 1px 1px 0.3px rgba(0,0,0,0.2);
-        display:flex;
-
-        form {
-            flex:1;
-            display: flex;
-
-            input, select {
-                height: 40px;
-                border: 0;
-                border-radius: 5px;
-                outline: 0;
-                font-size: 15px;
-                color: #000;
-                margin-right: 20px;
-            }
-
-            input {
-                flex: 1;
-                padding: 0 10px;
-            }
-
-            select {
-                width: 100px;
-            }
-
-            button {
-                background-color: #49AEEF;
-                font-size: 15px;
-                border: 0;
-                border-radius: 5px;
-                color: #FFF;
-                height: 40px;
-                padding: 0 20px;
-                cursor: pointer;
-            }
-        }
-    }
-
-    .categoryList {
-        display: flex;
-        flex-wrap: wrap;
-        margin-top: 20px;
-
-        .categoryItem {
-            width: 25%;
-            display: flex;
-            align-items: center;
-            color: #000;
-            text-decoration: none;
-            height: 50px;
-            justify-content: center;
-
-            &:hover {
-                color:#999;
-            }
-
-            img {
-                width: 30px;
-                height: 30px;
-                margin-right: 10px;
-            }
-
-            span {
-                min-width: 10ch;
-            }
-        }
-    }
-`;
-
 export const PageArea = styled.div`
+display: flex;
+margin-top: 20px;
 
-h2 {
-    font-size: 20px;
+.leftSide{
+    width: 250px;
+    margin-right: 10px;
+
+    .filterName{
+        font-size: 15px;
+        margin: 10px 0;
+    }
+
+    input, select {
+        width: 100%;
+        height: 40px;
+        border: 2px solid #9BBB3C;
+        border-radius: 5px;
+        outline: 0;
+        font-size: 15px;
+        color: #000;
+        padding: 10px;
+    }
+
+    ul, li{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+    .categoryItem{
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        border-radius: 5px;
+        color: #000;
+        cursor: pointer;
+
+        img{
+            width: 25px;
+            height: 25px;
+            margin: 5px;
+        }
+
+        span {
+            font-size: 14px;
+        }
+
+        &:hover, 
+        &.active{
+            background: #9BBB3C;
+            color: white;
+        }    
+    }
 }
 
-.list {
-    display: flex; 
-     flex-wrap: wrap;/*quera a linha */
-
-     .aditem {
-        width: 25%;
-     }
-}
-
-.tonho{
-    color: #000;
-    text-decoration: none;
-    font-weight: bold;
-    display: inline-block;
-    margin-top: 10px;
+.rightSide{
+    flex:1;
 }
 
 `;
