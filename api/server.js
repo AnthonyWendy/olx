4547debc5854e5ff68f3d72128afc495057321bd
Anthony <anthonywendyantuesdelima@gmail.com>
2022-07-19 12:27:18 -0300

    Server created and configured, ping/pong test created.

---
+++ api/server.js
@@ -0,0 +1,38 @@
+// Carregar as variavéis de ambiente.
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileupload = require('express-fileupload');
+
+// Conexão com o banco de dados
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParsel: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+mongoose.Promise = global.Promise; // O mogoose irá utilizar as promise locais
+
+mongoose.connection.on('error', (error)=>{//Menssagem de erro caso haja algum erro de conexão
+    console.log("Erro: ", error.message);
+});
+
+//Criando o servidor
+const server = express();
+
+server.use(cors());//Habilitar o recebimento de configurações padrão
+server.use(express.json());//O processp vai ser com o .json
+server.use(express.urlencoded({extended: true}));//
+server.use(fileupload());
+
+server.use(express.static(__dirname+'/public'));//Link que permite o acesso à pasta public
+
+//Criação das rotas
+server.get('ping', (req, res)=>{
+    res.json({pong: true});
+});
+
+server.listen(process.env.PORT, (=>{
+    console.log(`Rodando no endereço ${process.env.BASE}`)
+}))
