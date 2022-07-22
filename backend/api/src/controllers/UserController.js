f93fe902faf7b2288974ba5e0cf0d5c37598e8f8
AnthonyWendy <anthonywendyantuesdelima@gmail.com>
2022-07-21 21:02:10 -0300

    Method Signin and Signup created.

---
+++ backend/api/src/controllers/UserController.js
@@ -0,0 +1,17 @@
+const State = require('../models/State');
+
+// Responsável por controlar as informações do usuário
+module.exports = {
+    getStates: async (req, res) => {
+        let states = await State.find();//vai pegar todos os dados da minha tabela
+        res.json({states}); //vai mandar os dados
+    },
+  
+    info: async (req, res) => {
+        res.json({});
+    },
+
+    editAction: async (req, res) => {
+
+    }
+};
---
+++ backend/api/src/validators/AuthValidator.js
@@ -0,0 +1,40 @@
+const { checkSchema } = require('express-validator');
+
+module.exports = {
+    // Validar o cadastro
+    signup: checkSchema({
+        //Regras específicas
+        name: {
+            trim: true, //Vai retirar os espaços
+            isLength: {options: {min: 2}}, //Vai dizer que o min de caracteres é 
+            errorMessage: 'O seu nome precisa de no mínimo de caracteres.'
+
+        },
+        email: {
+            isEmail: true,//verifica se é email
+            normalizeEmail: true,//vai colocar o emaill prontinho para a inserção no banco
+            errorMessage: 'E-mail inválido.'
+        },
+        password: {
+            isLength:{ options: { min: 2} },
+            errorMessage: 'Senha precisa ter pelo menos 2 caracteres.'
+        },
+        state: {
+            notEmpty: true,
+            errorMessage: 'É necessário informar o estado.'
+        }
+    }),
+
+    signin: checkSchema({
+        email: {
+            isEmail: true,//verifica se é email
+            normalizeEmail: true,//vai colocar o emaill prontinho para a inserção no banco
+            errorMessage: 'E-mail inválido.'
+        },
+        password: {
+            isLength:{ options: { min: 2} },
+            errorMessage: 'Senha precisa ter pelo menos 2 caracteres.'
+        }
+    })
+
+}
