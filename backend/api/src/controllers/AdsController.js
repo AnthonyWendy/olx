c2f5932e6951777f19c9e6af6c6e4f8bd6c3c927
Anthony <anthonywendyantuesdelima@gmail.com>
2022-07-22 12:59:24 -0300

    Método de encontrar os anúncios criado.

--- backend/api/src/controllers/AdsController.js
+++ backend/api/src/controllers/AdsController.js
@@ -1,11 +1,25 @@
+const Category = require('../models/Category');
+
 // Responsável por controlar as alterações realizadas nos anúncios
 
 module.exports = {
     getCategories: async (req, res) => {
-        res.json({pong: true});
+        const cats = await Category.find();
+
+        let categories = [];
+
+        for(let i in cats){
+            categories.push({
+                ...cats[i]._doc,
+                img: `${process.env.BASE}/assets/images/${cats[i].slug}.png`
+            })
+        }
+
+        res.json({categories});
     },
     
     addAction: async (req, res) => {
+        //
 
     },
 
