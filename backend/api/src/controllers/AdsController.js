16ae7fa03f2e53ad73cf6469195467712d0286db
Anthony <anthonywendyantuesdelima@gmail.com>
2022-07-22 15:05:03 -0300

    Imgae upload method.

--- backend/api/src/controllers/AdsController.js
+++ backend/api/src/controllers/AdsController.js
@@ -1,9 +1,20 @@
+//Biblioteca para a manipulação de imagens
+const {v4: uuid} = require('uuid');
+const jimp = require('jimp');
+
 const User = require('../models/Users');
 const Category = require('../models/Category');
 const Ads = require('../models/Ads');
 
-// Responsável por controlar as alterações realizadas nos anúncios
+//Função que diminuir o tamanho da imagem, dá um nome para o arquivo e salva ela
+const addImage = async (buffer) => {
+    let newName = `${uuid()}.jpg`;
+    let tmpImg = await jimp.read(buffer);
+    tmpImg.cover(500, 500).quality(80).write(`./public/media/${newName}`);
+    return newName;
+}
 
+// Responsável por controlar as alterações realizadas nos anúncios
 module.exports = {
     getCategories: async (req, res) => {
         const cats = await Category.find();
@@ -24,7 +35,7 @@ module.exports = {
         //Método para adicionar novos anúncios
 
         //Pegas os dados do usuário
-        let {title, price, priceneg, desc, cat, token} = body;
+        let {title, price, priceneg, desc, cat, token} = req.body;
 
         const user = await User.findOne({token}).exec();
 
@@ -53,9 +64,42 @@ module.exports = {
         newAd.description = desc;
         newAd.views = 0;
 
+        //Verificando se foi enviado imagem
+        if(req.files && req.files.img){
+            //Verificando se foi enviado mais de duas imagens
+            if(req.files.img.length == undefined){
+                if(['image/jpeg', 'image/jpg', 'image/png'].includes(req.files.img.mimetype)){
+                    let url = await addImage(req.files.img.data);
+                    newAd.images.push({
+                        url,
+                        default: false
+                    });
+
+                }
+            }else {
+                for(let i=0; i < req.files.img.length; i++){
+                    if(['image/jpeg', 'image/jpg', 'image/png'].includes(req.files.img[i].mimetype)){
+                        let url = await addImage(req.files.img.data);
+                        newAd.images.push({
+                            url,
+                            default: false
+                        });
+                    }
+                }
+            }
+            //Coloca a primeira imagem como default para deixar ela como capa
+            if(newAd.images.length > 0){
+                newAd.images[0].default = true;
+            }
+        }
+
+        const info = await newAd.save();
+        res.json({id: info._id});
+
     },
 
     getList: async (req, res) => {
+        let 
 
     },
 
