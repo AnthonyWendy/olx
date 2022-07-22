4f3ca50d511e96aa7075de201b96219b84f4287e
Anthony <anthonywendyantuesdelima@gmail.com>
2022-07-22 16:06:11 -0300

    Contol method created.

--- backend/api/src/controllers/AdsController.js
+++ backend/api/src/controllers/AdsController.js
@@ -1,3 +1,5 @@
+const Ad = require('../models/Ads');
+
 //Biblioteca para a manipulação de imagens
 const {v4: uuid} = require('uuid');
 const jimp = require('jimp');
@@ -5,6 +7,7 @@ const jimp = require('jimp');
 const User = require('../models/Users');
 const Category = require('../models/Category');
 const Ads = require('../models/Ads');
+const StateModel = require('../models/State');
 
 //Função que diminuir o tamanho da imagem, dá um nome para o arquivo e salva ela
 const addImage = async (buffer) => {
@@ -99,8 +102,61 @@ module.exports = {
     },
 
     getList: async (req, res) => {
-        let 
+        let { sort = 'asc', offset = 0, limit = 8, q, cat, state } = req.query;
+        let filters = {status: true};
+        let total = 0;
 
+        //filtros
+        if(q){//por palavra
+            filters.title = {'$regegx': q, 'options': 'i'};
+        }
+
+        if(cat){//por categoria
+            const c = await Category.findOne({slug: cat}).exec();
+            if(c){
+                filters.category = c._id.toString();
+            }
+        }
+
+        if(state){
+            const s = await StateModel.findOne({name: state.toUpperCase()}).exec();
+            if(s){
+                filters.state = s._id.toString();
+            }
+        }
+
+        //mostrando o total de itens
+        const adsTotal = await Ad.find(filters).exec();
+        total = adsTotal.length;
+
+        //mostrando os resultados
+        const adsData = await Ad.find(filters)
+                                .sort({dateCreated: (sort == 'des' ? -1:1)})
+                                .skip(parseInt(offset))
+                                .limit(parseInt(limit))
+                                .exec();
+
+        let ads = [];
+        for(let i in adsData){
+            let image;
+            let defaultImg = adsData[i].images.find(e => e.default);
+            if(defaultImg){
+                image = `${process.env.BASE}/media/${defaultImg.url}`
+            }else {
+                image = `${process.env.BASE}/media/deafult.img`
+            }
+            ads.push({
+                id: adsData._id,
+                title: adsData.title,
+                price: adsData.price,
+                priceNegotiable: adsData[i].priceNegotiable,
+                image
+            })
+        }
+
+
+
+       res.json({ads, total});
     },
 
     getItem: async (req, res) => {
